1c2c4b4093f74a2783cae11082ef2a8493bd3abd
igor.ptz@hotmail.com <igor.ptz@hotmail.com>
2025-12-04 11:13:01 -0300

    Update

---
+++ src/lib/auth0Config.ts
@@ -0,0 +1,7 @@
+export const auth0Config = {
+    clientId: "9v1jKS4CCcXlwiBjbPbHkimWhq2VSuUA",
+    issuer: "dev-rbc0r7uw3fhqc8bx.us.auth0.com",
+    audience: "http://localhost:8080",
+    redirectUri: window.location.origin + "/callback",
+    scope: "openid profile email"
+}
